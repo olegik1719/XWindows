51227bb8de5568a7bf2626403dfb51f1968b6333
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-09 14:49:20 +0300

    Скопировано и немного подправлено.

---
+++ src/main/java/CrowsCalc.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+//import java.awt.event.*;
+import javax.swing.*;
+
+public class CrowsCalc extends JFrame {
+    private int crows = 0;
+    private JLabel countLabel;
+    private JButton addCrow;
+    private JButton removeCrow;
+
+    public CrowsCalc(){
+        super("Crow calculator");
+        //Подготавливаем компоненты объекта
+        countLabel = new JLabel("Crows:" + crows);
+        addCrow = new JButton("Add Crow");
+        removeCrow = new JButton("Remove Crow");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        //Подготавливаем временные компоненты
+        JPanel buttonsPanel = new JPanel(new FlowLayout());
+        //Расставляем компоненты по местам
+        add(countLabel, BorderLayout.NORTH); //О размещении компонент поговорим позже
+
+
+        buttonsPanel.add(addCrow);
+        buttonsPanel.add(removeCrow);
+
+        add(buttonsPanel, BorderLayout.SOUTH);
+        initListeners();
+    }
+
+    private void initListeners() {
+        addCrow.addActionListener(e -> {
+            crows = crows + 1;   /* Добавляем одну ворону  */
+            updateCrowCounter(); /* Сообщаем аппликации, что количество ворон изменилось  */
+        });
+        removeCrow.addActionListener(e -> {
+            if (crows > 0) {
+                crows = crows - 1;
+                updateCrowCounter(); /* Сообщаем аппликации, что количество ворон изменилось  */
+            } else{
+                JOptionPane.showMessageDialog(this,
+                        "Crows are finished yet!");
+            }
+        });
+    }
+    private void updateCrowCounter() {
+        countLabel.setText("Crows:" + crows);
+    }
+
+    public static void main(String[] args) {
+        CrowsCalc app = new CrowsCalc();
+        app.setVisible(true);
+        app.pack(); //Эта команда подбирает оптимальный размер в зависимости от содержимого окна
+    }
+}
